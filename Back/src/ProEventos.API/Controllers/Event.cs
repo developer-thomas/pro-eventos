748493750e54f6dacfb6d81c5fa210929182e642
Thomas Edson <andepipous@gmail.com>
2026-06-20 15:19:07 -0300

    feat: adding initial structure on backend

---
+++ Back/src/ProEventos.API/Controllers/Event.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventController : ControllerBase
+    {
+        
+        private IEnumerable<Event> _event = new Event[]
+        {
+            new Event()
+            {
+                EventId = 1,
+                Theme = "Angular",
+                Place = "Santa Catarina",
+                Lote = "1º Lote",
+                PeopleQuantity = 250,
+                EventDate = DateTime.Now.AddDays(2).ToString(),
+                ImageUrl = ""
+            },
+            new Event()
+            {
+                EventId = 2,
+                Theme = "Dotnet",
+                Place = "Palhoça",
+                Lote = "2º Lote",
+                PeopleQuantity = 250,
+                EventDate = DateTime.Now.AddDays(3).ToString(),
+                ImageUrl = ""
+            },
+        };
+
+        public EventController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Event> Get()
+        {
+            return _event;
+        }
+        
+        [HttpGet("{id}")]
+        public IEnumerable<Event> GetById(int id)
+        {
+            return _event.Where(eventCallback => eventCallback.EventId == id);
+        }
+    }
+}
---
+++ Back/src/ProEventos.API/Models/Event.cs
@@ -0,0 +1,14 @@
+﻿namespace ProEventos.API.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string Place  { get; set; }
+        public string EventDate { get; set; }
+        public string Theme { get; set; }
+        public int PeopleQuantity { get; set; }
+        public string Lote { get; set; }
+        public string ImageUrl { get; set; }
+    
+    }    
+}
